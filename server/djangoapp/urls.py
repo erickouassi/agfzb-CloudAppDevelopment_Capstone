77e421009e6d8a26ad44f1b8a6fae68269055e1a
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2022-04-03 00:00:06 -0400

    Update urls

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,17 +17,18 @@ urlpatterns = [
 
     # path for registration
     path('registration/', views.registration_request, name='registration'),
+    
     # path for login
     path('login/', views.login_request, name='login'),
 
     # path for logout
-     path('logout/', views.logout_request, name='logout'),
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
     path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-    
+
     # path for add a review view
     path('dealer/<int:dealer_id>/add_review', views.add_review, name='add_review'),
 
