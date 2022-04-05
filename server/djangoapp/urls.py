8b276ceb7eb690bc67846b795b3f80a075a87437
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2022-04-04 20:07:21 -0400

    Update code

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
 
     # path for logout
     path('logout/', views.logout_request, name='logout'),
-
+    
+    # path for landing page
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
