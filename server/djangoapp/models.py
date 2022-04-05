9b8ea9b4138ab4a1623cd2657edc640274e0b5c9
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2022-04-04 21:20:05 -0400

    Update and cleaning up code

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -12,14 +12,13 @@ import uuid
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 class CarMake(models.Model):
-
+#    id = models.AutoField(primary_key=True)
     name = models.CharField(null=False, max_length=30, default='car make')
     description = models.CharField(max_length=1000)
 
     def __str__(self):
         return "Name: " + self.name + "," + \
                "Description: " + self.description
-
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -30,13 +29,13 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    SEDAN = 'Sedan'
-    SUV = 'Suv'
-    TRUCK = 'Truck'
+    SEDAN = 'sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
     CAR_TYPES = [
-        (SEDAN, 'SEDAN'),
+        (SEDAN, 'sedan'),
         (SUV, 'SUV'),
-        (TRUCK, 'TRUCK')
+        (WAGON, 'WAGON')
     ]
 #    id = models.AutoField(primary_key=True)
     make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -53,7 +52,17 @@ class CarModel(models.Model):
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 class CarDealer:
-    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip, state):
+
+    def __init__(self, 
+                 address, 
+                 city, 
+                 full_name, 
+                 id, 
+                 lat, 
+                 long, 
+                 short_name, 
+                 st, 
+                 zip):
         # Dealer address
         self.address = address
         # Dealer city
@@ -93,7 +102,7 @@ class DealerReview:
         # Review
         self.review = review
         # Purchase Date
-        self.purchase_date = '12/31/9999'
+        self.purchase_date = '99/99/9999'
         # Car Make
         self.car_make = 'N/A'
         # Car Model
